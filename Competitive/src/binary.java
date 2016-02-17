5f0d88849aaee4ca84da8709b2e0ba007ad780a6
oakhan3 <oakhan3@gmail.com>
2016-02-16 19:46:36 -0500

    Template

--- Competitive/src/binary.java
+++ Competitive/src/binary.java
@@ -7,11 +7,132 @@ public class binary {
 		while (scan.hasNext()) {
 			String bin1 = scan.next();
 			String bin2 = scan.next();
+			
+			int n = Math.max(bin1.length(), bin2.length()) + 1;
+			
+			if (bin1.length() > bin2.length()) {
+				StringBuilder buffer = new StringBuilder();
+				for (int i = 0; i < bin1.length() - bin2.length() + 1; i++)
+					buffer.append(' ');
+				buffer.append(bin2);
+				bin2 = buffer.toString();
+				bin1 = " " + bin1;
+			}
+			else if (bin2.length() > bin1.length()) {
+				StringBuilder buffer = new StringBuilder();
+				for (int i = 0; i < bin2.length() - bin1.length() + 1; i++)
+					buffer.append(' ');
+				buffer.append(bin1);
+				bin1 = buffer.toString();
+				bin2 = " " + bin2;
+			}
+			else {
+				bin1 = " " + bin1;
+				bin2 = " " + bin2;
+			}
+			
+			char[][] print = new char[2][n];
+			
+			for(int i = 0; i < n; i++) {
+				print[0][i] = ' ';
+				print[1][i] = ' ';
+				
+			}
+
+			/*
+			 * 	0 + 0 + 0 ===> 0 with NO carry DONE
+				0 + 0 + 1 ===> 1 with NO carry DONE
+				0 + 1 + 0 ===> 1 with NO carry DONE
+				0 + 1 + 1 ===> 0 with    carry
+				1 + 0 + 0 ===> 1 with NO carry
+				1 + 0 + 1 ===> 0 with    carry
+				1 + 1 + 0 ===> 0 with    carry
+				1 + 1 + 1 ===> 1 with    carry
+			 */
+			
+			int j = n - 1;
+			
+			while (j != 0) {
+				if ((bin1.charAt(j) == '0' || bin1.charAt(j) == ' ') 
+				 && (bin2.charAt(j) == '0' || bin2.charAt(j) == ' ') 
+				 && print[0][j] == ' ') 
+				{
+					print[0][j - 1] = ' ';
+					print[1][j] = '0';
+				}
+				else if ((bin1.charAt(j) == '0' || bin1.charAt(j) == ' ') 
+					  && (bin2.charAt(j) == '0' || bin2.charAt(j) == ' ') 
+					  && print[0][j] == '1') 
+				{
+					print[0][j - 1] = ' '; //Carry
+					print[1][j] = '1'; //Sum
+				}
+				else if ((bin1.charAt(j) == '0' || bin1.charAt(j) == ' ') 
+					   && bin2.charAt(j) == '1' 
+					   && print[0][j] == ' ') 
+				{
+					print[0][j - 1] = ' '; //Carry
+					print[1][j] = '1'; //Sum
+				}
+				else if (bin1.charAt(j) == '1' 
+				     && (bin2.charAt(j) == '0' || bin2.charAt(j) == ' ') 
+				     && print[0][j] == ' ') 
+				{
+					print[0][j - 1] = ' '; //Carry
+					print[1][j] = '1'; //Sum
+				}
+				else if (bin1.charAt(j) == '1' 
+					 && (bin2.charAt(j) == '0' || bin2.charAt(j) == ' ') 
+					 && print[0][j] == '1') 
+				{
+					print[0][j - 1] = '1'; //Carry
+					print[1][j] = '0'; //Sum
+				}
+				else if (bin1.charAt(j) == '1' 
+					  && bin2.charAt(j) == '1' 
+					  && print[0][j] == ' ') 
+				{
+					print[0][j - 1] = '1'; //Carry
+					print[1][j] = '0'; //Sum
+				}
+				else if (bin1.charAt(j) == '1' 
+					  && bin2.charAt(j) == '1' 
+					  && print[0][j] == '1') 
+				{
+					print[0][j - 1] = '1'; //Carry
+					print[1][j] = '1'; //Sum
+				}
+				else
+					print[1][j] = 'x';
+				j--;
+			}
+			
+			print[1][0] = (print[0][0] == '1') ? '1' : '0';
+			
+			System.out.println("BINARY SUM:");
+			
+			for (int i = 0; i < n; i++) {
+				System.out.print(print[0][i]);
+			}
+			System.out.println();
+			
 			System.out.println(bin1);
 			System.out.println(bin2);
 			
+			for (int i = 0; i < n; i++) {
+				System.out.print("-");
+			}
 			
+			System.out.println();
+			
+			for (int i = 0; i < n; i++) {
+				System.out.print(print[1][i]);
+			}
+			
+			System.out.println("\n");
+						
 		}
+		
 		scan.close();
 	}
 }
