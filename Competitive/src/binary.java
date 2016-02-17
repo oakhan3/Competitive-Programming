324636e24f818c60bcea8f0c51e4f7905b4a93cd
oakhan3 <oakhan3@gmail.com>
2016-02-16 22:32:38 -0500

    Finished

--- Competitive/src/binary.java
+++ Competitive/src/binary.java
@@ -5,11 +5,15 @@ public class binary {
 		Scanner scan = new Scanner(System.in);
 		
 		while (scan.hasNext()) {
+			
 			String bin1 = scan.next();
+			if (scan.hasNext()) {
+			
 			String bin2 = scan.next();
 			
 			int n = Math.max(bin1.length(), bin2.length()) + 1;
 			
+			//Append spaces to even out each line length to (n - 1)
 			if (bin1.length() > bin2.length()) {
 				StringBuilder buffer = new StringBuilder();
 				for (int i = 0; i < bin1.length() - bin2.length() + 1; i++)
@@ -31,108 +35,71 @@ public class binary {
 				bin2 = " " + bin2;
 			}
 			
+			//Create a 2D Array for Sum and Carry
 			char[][] print = new char[2][n];
 			
 			for(int i = 0; i < n; i++) {
 				print[0][i] = ' ';
 				print[1][i] = ' ';
+			}
+			
+			//Calculate Sum and Carry
+			int k = 0;
+			
+			for (int j = n - 1; j > 0; j--) {
+				if (print[0][j] == '1')
+					k++;
+				if (bin1.charAt(j) == '1')
+					k++;
+				if (bin2.charAt(j) == '1')
+					k++;
 				
-			}
-
-			/*
-			 * 	0 + 0 + 0 ===> 0 with NO carry DONE
-				0 + 0 + 1 ===> 1 with NO carry DONE
-				0 + 1 + 0 ===> 1 with NO carry DONE
-				0 + 1 + 1 ===> 0 with    carry
-				1 + 0 + 0 ===> 1 with NO carry
-				1 + 0 + 1 ===> 0 with    carry
-				1 + 1 + 0 ===> 0 with    carry
-				1 + 1 + 1 ===> 1 with    carry
-			 */
-			
-			int j = n - 1;
-			
-			while (j != 0) {
-				if ((bin1.charAt(j) == '0' || bin1.charAt(j) == ' ') 
-				 && (bin2.charAt(j) == '0' || bin2.charAt(j) == ' ') 
-				 && print[0][j] == ' ') 
-				{
-					print[0][j - 1] = ' ';
+				if (k == 3) {
+					print[1][j] = '1';
+					print[0][j - 1] = '1';
+				}
+				else if (k == 2) {
 					print[1][j] = '0';
+					print[0][j - 1] = '1';
 				}
-				else if ((bin1.charAt(j) == '0' || bin1.charAt(j) == ' ') 
-					  && (bin2.charAt(j) == '0' || bin2.charAt(j) == ' ') 
-					  && print[0][j] == '1') 
-				{
-					print[0][j - 1] = ' '; //Carry
-					print[1][j] = '1'; //Sum
+				else if (k == 1) {
+					print[1][j] = '1';
+					print[0][j - 1] = ' ';
 				}
-				else if ((bin1.charAt(j) == '0' || bin1.charAt(j) == ' ') 
-					   && bin2.charAt(j) == '1' 
-					   && print[0][j] == ' ') 
-				{
-					print[0][j - 1] = ' '; //Carry
-					print[1][j] = '1'; //Sum
+				else {
+					print[1][j] = '0';
+					print[0][j - 1] = ' ';
 				}
-				else if (bin1.charAt(j) == '1' 
-				     && (bin2.charAt(j) == '0' || bin2.charAt(j) == ' ') 
-				     && print[0][j] == ' ') 
-				{
-					print[0][j - 1] = ' '; //Carry
-					print[1][j] = '1'; //Sum
-				}
-				else if (bin1.charAt(j) == '1' 
-					 && (bin2.charAt(j) == '0' || bin2.charAt(j) == ' ') 
-					 && print[0][j] == '1') 
-				{
-					print[0][j - 1] = '1'; //Carry
-					print[1][j] = '0'; //Sum
-				}
-				else if (bin1.charAt(j) == '1' 
-					  && bin2.charAt(j) == '1' 
-					  && print[0][j] == ' ') 
-				{
-					print[0][j - 1] = '1'; //Carry
-					print[1][j] = '0'; //Sum
-				}
-				else if (bin1.charAt(j) == '1' 
-					  && bin2.charAt(j) == '1' 
-					  && print[0][j] == '1') 
-				{
-					print[0][j - 1] = '1'; //Carry
-					print[1][j] = '1'; //Sum
-				}
-				else
-					print[1][j] = 'x';
-				j--;
+				k = 0;	
 			}
 			
+			//Append the first number to the Sum
 			print[1][0] = (print[0][0] == '1') ? '1' : '0';
 			
+			//Print out the Calculation following Specific Format
 			System.out.println("BINARY SUM:");
 			
-			for (int i = 0; i < n; i++) {
-				System.out.print(print[0][i]);
+			for (int q = 0; q < n; q++) {
+				System.out.print(print[0][q]);
 			}
 			System.out.println();
 			
 			System.out.println(bin1);
 			System.out.println(bin2);
 			
-			for (int i = 0; i < n; i++) {
+			for (int m = 0; m < n; m++) {
 				System.out.print("-");
 			}
 			
 			System.out.println();
 			
-			for (int i = 0; i < n; i++) {
-				System.out.print(print[1][i]);
+			for (int p = 0; p < n; p++) {
+				System.out.print(print[1][p]);
 			}
 			
 			System.out.println("\n");
-						
+			}
 		}
-		
 		scan.close();
 	}
 }
