f967f911d7aba9e16377345352e1f089b21d637d
Omar <oakhan3@gmail.com>
2016-03-23 18:34:17 -0400

    imp objects

--- Competitive/src/lilBishopsDiags.java
+++ Competitive/src/lilBishopsDiags.java
@@ -6,7 +6,7 @@ public class lilBishopsDiags {
 	//k is # of bishops
 	private static int n, k, solution;
 
-	private static void backtrack(int bish, int x, int y, Hashtable<String, Boolean> topLeft, Hashtable<String, Boolean> topRight) {
+	private static void backtrack(int bish, int x, int y, Hashtable<Double, Boolean> topLeft, Hashtable<Double, Boolean> topRight) {
 		//Move on to the next position without placing a bishop this time
 		if (x < (n - 1))
 			backtrack(bish, x + 1, y, topLeft, topRight);
@@ -23,32 +23,35 @@ public class lilBishopsDiags {
 			c--;
 			d++;
 		}
+		
+		double left = .5*(a + b)*(a + b + 1) + b;
+		double right = .5*(c + d)*(c + d + 1) + d;
 				
 		//If you can possibly place a Bishop on attempt[x][y]
-		if (topLeft.get(a+"-"+b) == false && topRight.get(c+"-"+d) == false) {
+		if (topLeft.get(left) == false && topRight.get(right) == false) {
 			//Place the Bishop by marking the diagonals from position x, y
 			//If b + 1 = the number of bishops wanted, add to the solutions list
 			//else carry on by 'backtracking' forward
 			if (bish + 1 == k)
 				solution++;
 			else {
-				topLeft.put(a+"-"+b, true);
-				topRight.put(c+"-"+d, true);
+				topLeft.put(left, true);
+				topRight.put(right, true);
 				
 				if (x < (n - 1))
 					backtrack(bish + 1, x + 1, y, topLeft, topRight);
 				else if (y < (n - 1)) 
 					backtrack(bish + 1, 0, y + 1, topLeft, topRight);
 				
-				topLeft.put(a+"-"+b, false);
-				topRight.put(c+"-"+d, false);
+				topLeft.put(left, false);
+				topRight.put(right, false);
 			}
 		}
 	}
 	
 	private static void setup() {
-		Hashtable<String, Boolean> topLeft  = new Hashtable<String, Boolean>();
-		Hashtable<String, Boolean> topRight = new Hashtable<String, Boolean>();
+		Hashtable<Double, Boolean> topLeft  = new Hashtable<Double, Boolean>();
+		Hashtable<Double, Boolean> topRight = new Hashtable<Double, Boolean>();
 		
 		int a = n-1;
 		int b = 0;
@@ -58,7 +61,7 @@ public class lilBishopsDiags {
 				a = 0;
 			if (i > ((n*2)-1)/2)
 				b++;
-			topLeft.put(a+"-"+b, false);
+			topLeft.put(.5*(a + b)*(a + b + 1) + b, false);
 			a--;
 		}
 		
@@ -68,23 +71,10 @@ public class lilBishopsDiags {
 		for (int i = 0; i < (n*2)-1; i++) {
 			if (i > ((n*2)-1)/2)
 				a++;
-			topRight.put(a+"-"+b, false);
+			topRight.put(.5*(a + b)*(a + b + 1) + b, false);
 			if (b != (n-1))
 				b++;
 		}
-		
-		/*System.out.println("Top Left");
-		
-		for (String i : topLeft.keySet())
-			System.out.print(i + " ");
-		
-		System.out.println("\nTop Right");
-		
-		for (String i : topRight.keySet())
-			System.out.print(i + " ");
-		
-		System.out.println();
-		*/
 		
 		backtrack(0, 0, 0, topLeft, topRight);
 		
